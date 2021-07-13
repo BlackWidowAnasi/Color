174959e1db716b399b9633ca3d328f6f875bf56e
BlackWidowAnasi <87397602+BlackWidowAnasi@users.noreply.github.com>
2021-07-13 15:58:19 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 var ball;
-
+var canvas
 
 function setup() {
   createCanvas(600,300);
+  canvas = createSprite(600,300, 1500, 1000);
   ball = createSprite(150,150, 15, 15);
-
 }
 
 function draw() 
@@ -26,6 +26,21 @@ function draw()
     ball.shapeColor = "indigo";
   }
 
+  if (keyIsDown(UP_ARROW)){
+    canvas.shapeColor = "black";
+  }
+
+  if (keyIsDown(DOWN_ARROW)){
+    canvas.shapeColor = "indigo";
+  }
+
+  if (keyIsDown(RIGHT_ARROW)){
+    canvas.shapeColor = "red";
+  }
+
+  if (keyIsDown(LEFT_ARROW)){
+    canvas.shapeColor = "white";
+  }
   drawSprites()
 }
 
